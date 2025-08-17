0e8765f6bc7a3c7cb7445c42dcef988262850007
DaviGGA <davigregorio.a@hotmail.com>
2025-08-17 19:13:51 -0300

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,29 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+const server = net.createServer((socket: net.Socket) => {
+  socket.on("data", data => {
+    const command = data.toString();
+
+    if (command === "PING") {
+      socket.write("+PONG\r\n");
+      return;
+    }
+
+    socket.write("Oops!");
+
+  })
+
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6380, "127.0.0.1");
+
+const client = net.connect({port: 6380}, () => {
+  const command = prompt("");
+  client.write(command!);
+})
+
+client.on("data", data => {
+  console.log(data.toString("utf-8"));
+})
+
