15e8ecf4d1130b1b327110698352ee48d90374d7
DaviGGA <davigregorio.a@hotmail.com>
2025-08-22 20:06:47 -0300

    [any message]

--- app/packages/commands/lpop.ts
+++ app/packages/commands/lpop.ts
@@ -4,6 +4,7 @@ import { array, bulkString } from "../Resp/data-types";
 export function lpop(key: string, quantity: number) {
   const lpopResult = MemoryStorage.lpop(key, quantity);
   console.log("LPOP RESULT", lpopResult);
+  console.log("LPOP RESP ARR", array(lpopResult))
   return lpopResult.length === 0 ?
     array(lpopResult) : bulkString("");
 }
