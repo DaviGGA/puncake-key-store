85b9733784985079f413d70f701ce9aefcc94a13
DaviGGA <davigregorio.a@hotmail.com>
2025-08-23 00:58:22 -0300

    [any message]

--- app/packages/commands/blpop.ts
+++ app/packages/commands/blpop.ts
@@ -8,35 +8,51 @@ type Blpop = {
   socketId: number
 }
 
+type Miliseconds = number;
+const toSeconds = (ms: Miliseconds) => ms * 1000;
+
 export const blpopListeners: Map<number, any> = new Map();
 
 export async function blpop({key, timeout, socketId}: Blpop): Promise<string> {
-  //const parsedTimeout = parseInt(timeout);
+  const parsedTimeout = toSeconds(parseInt(timeout));
 
   const lpopResult = MemoryStorage.lpop(key, 1);
 
   if (lpopResult.length > 0) 
     return getResult(key, lpopResult);
 
-return new Promise(resolve => {
-  function onElementAdded(addedKey: string) {
-    if (addedKey != key) return;
+  const resultPromise: Promise<string> = new Promise(resolve => {
+    function onElementAdded(addedKey: string) {
+      console.log("chamando")
+      if (addedKey != key) return;
 
-    const lpopResult = MemoryStorage.lpop(key, 1);
-    if (lpopResult.length === 0) return;
+      const lpopResult = MemoryStorage.lpop(key, 1);
+      if (lpopResult.length === 0) return;
 
-    blpopListeners.delete(socketId);
-    eventEmitter.off("ELEMENT_ADDED", wrappedFn);
-    
-    resolve(getResult(key, lpopResult));
-  }
+      blpopListeners.delete(socketId);
+      eventEmitter.off("ELEMENT_ADDED", wrappedFn);
+      
+      resolve(getResult(key, lpopResult));
+    }
 
-  const wrappedFn = (addedKey: string) => onElementAdded(addedKey);
+    const wrappedFn = (addedKey: string) => onElementAdded(addedKey);
 
-  blpopListeners.set(socketId, wrappedFn);
+    blpopListeners.set(socketId, wrappedFn);
 
-  eventEmitter.on("ELEMENT_ADDED", wrappedFn);
-});
+    eventEmitter.on("ELEMENT_ADDED", wrappedFn);
+  });
+
+  const timeoutPromise: Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+      const listener = blpopListeners.get(socketId);
+      if(!listener) return;
+      blpopListeners.delete(socketId);
+      eventEmitter.off("ELEMENT_ADDED", listener); 
+      resolve(bulkString(""))
+    }, parsedTimeout)
+  })
+
+return Promise.race([resultPromise, timeoutPromise])
 
 }
 
